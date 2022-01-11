927dda9556290d3de14c242626c1d2d30e1da5f0
Deenadhayalan17 <deena17698ddd@gmail.com>
2022-01-11 23:22:22 +0530

    completed Even the Last

--- Home/2.Even-The-Last.py
+++ Home/2.Even-The-Last.py
@@ -21,3 +21,28 @@
 # Precondition: 0 ≤ len(array) ≤ 20
 # all(isinstance(x, int) for x in array)
 # all(-100 < x < 100 for x in array)
+
+def checkio(array: list) -> int:
+    """
+        sums even-indexes elements and multiply at the last
+    """
+    if len(array) == 0:
+        return 0
+    else:
+        a = array[::2]
+        b = array[-1]        
+        c = 0
+        for i in a:
+            c = c + i
+        return c*b
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    print('Example:')
+    print(checkio([0, 1, 2, 3, 4, 5]))
+    
+    assert checkio([0, 1, 2, 3, 4, 5]) == 30, "(0+2+4)*5=30"
+    assert checkio([1, 3, 5]) == 30, "(1+5)*5=30"
+    assert checkio([6]) == 36, "(6)*6=36"
+    assert checkio([]) == 0, "An empty array = 0"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
