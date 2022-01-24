11abecb521b4541e6937c1aae8979991b56d1ae5
Deenadhayalan17 <deena17698ddd@gmail.com>
2022-01-24 10:40:45 +0530

    added solution

--- Home/8.Backward Each Word.py
+++ Home/8.Backward Each Word.py
@@ -15,3 +15,31 @@
 # 3
 # 4
 # Precondition: The line consists only from alphabetical symbols and spaces.
+
+# def backward_string_by_word(text: str) -> str:
+#     # your code here
+#     final=''
+#     list = text.split()
+#     for i in list:
+#       if final == '':
+        
+#           final=  final + (i[::-1])
+#       else:
+#         final=  final + ' '+ (i[::-1])
+        
+#     return final
+def backward_string_by_word(text: str) -> str:
+    # your code here
+    
+     return " ".join([i[::-1] for i in text.split(" ")])
+if __name__ == '__main__':
+    print("Example:")
+    print(backward_string_by_word(''))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert backward_string_by_word('') == ''
+    assert backward_string_by_word('world') == 'dlrow'
+    assert backward_string_by_word('hello world') == 'olleh dlrow'
+    assert backward_string_by_word('hello   world') == 'olleh   dlrow'
+    assert backward_string_by_word('welcome to a game') == 'emoclew ot a emag'
+    print("Coding complete? Click 'Check' to earn cool rewards!")
