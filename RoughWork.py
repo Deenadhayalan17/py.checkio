a3a05c4054af9fb9b9881c8a4fe5aab229f5db41
Deenadhayalan17 <deena17698ddd@gmail.com>
2022-02-10 20:07:31 +0530

    completed split list

--- RoughWork.py
+++ RoughWork.py
@@ -1,13 +1,6 @@
-# h = 7
-# m = 0
-# time = "07:00"
-# b = list(time.split(':')
+a = 5
+b = a//2
+print(b)
 
-
-# a=15 * h + m / 4 - 90
-# print(b)
-
-h = 7
-m = 15
-a = 15 * (h-6) + m / 4
-print(a)
+# if a % 2 == 0:
+#     print(a)
