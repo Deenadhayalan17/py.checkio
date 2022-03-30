ab64fe0470fe2a9ce37ee348bff22259f21e6509
Deenadhayalan17 <deena17698ddd@gmail.com>
2022-03-30 17:39:11 +0530

    added 4

---
+++ Electronic Station/4.Acceptable Password II.py
@@ -0,0 +1,23 @@
+# In this mission you need to create a password verification function.
+
+# Those are the verification conditions:
+
+# the length should be bigger than 6;
+# should contain at least one digit.
+# Input: A string.
+
+# Output: A bool.
+
+# Example:
+
+# is_acceptable_password('short') == False
+# is_acceptable_password('muchlonger') == False
+# is_acceptable_password('ashort') == False
+# is_acceptable_password('muchlonger5') == True
+# is_acceptable_password('sh5') == False
+# 1
+# 2
+# 3
+# 4
+# 5
+# How it’s used: For password verification form. Also it's good to learn how the task can be evaluated.
