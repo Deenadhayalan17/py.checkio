fbd6e7c6166e48c3ad699e0b3edd27753ed8a0f6
Deenadhayalan17 <deena17698ddd@gmail.com>
2022-03-31 17:33:28 +0530

    pre req

--- Electronic Station/4.Acceptable Password II.py
+++ Electronic Station/4.Acceptable Password II.py
@@ -21,3 +21,12 @@
 # 4
 # 5
 # How it’s used: For password verification form. Also it's good to learn how the task can be evaluated.
+
+if __name__ == "__main__":
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password("short") == False
+    assert is_acceptable_password("muchlonger") == False
+    assert is_acceptable_password("ashort") == False
+    assert is_acceptable_password("muchlonger5") == True
+    assert is_acceptable_password("sh5") == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
