0ae97922806fae09ca3253cae5dc549c99769fb6
Deenadhayalan17 <deena17698ddd@gmail.com>
2022-03-31 17:54:57 +0530

    Added 4.Acceptable Password II

--- Electronic Station/4.Acceptable Password II.py
+++ Electronic Station/4.Acceptable Password II.py
@@ -22,6 +22,31 @@
 # 5
 # How it’s used: For password verification form. Also it's good to learn how the task can be evaluated.
 
+# Taken from mission Acceptable Password I
+
+def is_acceptable_password(password: str) -> bool:
+    # your code here
+    if len(password) > 6:
+        a = False
+        for i in password:
+            if i.isdigit():
+                a = True
+        return a
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_acceptable_password('short'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password('short') == False
+    assert is_acceptable_password('muchlonger') == False
+    assert is_acceptable_password('ashort') == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
 if __name__ == "__main__":
     # These "asserts" are used for self-checking and not for an auto-testing
     assert is_acceptable_password("short") == False
