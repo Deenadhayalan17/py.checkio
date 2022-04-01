c9b32fdb8cf95f2ffd3f0ab2149e30befeba7b35
Deenadhayalan17 <deena17698ddd@gmail.com>
2022-04-01 13:14:23 +0530

    added 5

--- Electronic Station/5.Acceptable Password III.py
+++ Electronic Station/5.Acceptable Password III.py
@@ -16,12 +16,19 @@
 # is_acceptable_password('muchlonger5') == True
 # is_acceptable_password('sh5') == False
 # is_acceptable_password('1234567') == False
-if __name__ == "__main__":
-    # These "asserts" are used for self-checking and not for an auto-testing
-    assert is_acceptable_password("short") == False
-    assert is_acceptable_password("muchlonger") == False
-    assert is_acceptable_password("ashort") == False
-    assert is_acceptable_password("muchlonger5") == True
-    assert is_acceptable_password("sh5") == False
-    assert is_acceptable_password("1234567") == False
-    print("Coding complete? Click 'Check' to earn cool rewards!")
+# Taken from mission Acceptable Password II
+
+# Taken from mission Acceptable Password I
+
+def is_acceptable_password(password: str) -> bool:
+    # your code here
+    if (password.isdigit()):
+        return False
+    elif len(password) > 6:
+        a = False
+        for i in password:
+            if i.isdigit():
+                a = True
+        return a
+    else:
+        return False
