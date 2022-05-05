9715529964c95bc0a08daa324a2d5a8d82ef8d4d
Deenadhayalan17 <deena17698ddd@gmail.com>
2022-05-05 18:00:20 +0530

    completed bird language

--- Scientific Expedition/6.Bird Language.py
+++ Scientific Expedition/6.Bird Language.py
@@ -25,3 +25,32 @@
 
 # Precondition: re.match("\A([a-z]+\ ?)+(?<!\ )\Z", phrase)
 # A phrase always has the translation.
+
+
+def translate(text: str) -> str:
+    # your code here
+    v = "aeiouy"
+    finaltext = []
+    i = 0
+    while i < len(text):
+        finaltext.append(text[i])
+        if text[i] in v:
+            i += 3
+        elif text[i] == " ":
+            i += 1
+        else:
+            i += 2
+    print(finaltext)
+    return ''.join(finaltext)
+
+
+if __name__ == "__main__":
+    print("Example:")
+    print(translate("hieeelalaooo"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert translate("hieeelalaooo") == "hello"
+    assert translate("hoooowe yyyooouuu duoooiiine") == "how you doin"
+    assert translate("aaa bo cy da eee fe") == "a b c d e f"
+    assert translate("sooooso aaaaaaaaa") == "sos aaa"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
