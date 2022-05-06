5e4fedd547ed5b6890162b1c786312b2b3f37c61
Deenadhayalan17 <deena17698ddd@gmail.com>
2022-05-06 18:36:33 +0530

    completed

--- Scientific Expedition/8.Follow Instructions.py
+++ Scientific Expedition/8.Follow Instructions.py
@@ -25,3 +25,34 @@
 # How it is used: In games with a map
 
 # Precondition: only chars f,b,l and r are allowed
+
+
+from typing import Tuple
+
+
+def follow(instructions: str) -> Tuple[int, int]:
+    # your code here
+    x = 0
+    y = 0
+    for i in instructions:
+        if i == "l":
+            x += -1
+        elif i == "f":
+            y += 1
+        elif i == "r":
+            x += 1
+        elif i == "b":
+            y += -1
+
+    return (x, y)
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(follow("fflff"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert follow("fflff") == (-1, 4)
+    assert follow("ffrff") == (1, 4)
+    assert follow("fblr") == (0, 0)
+    print("Coding complete? Click 'Check' to earn cool rewards!")
